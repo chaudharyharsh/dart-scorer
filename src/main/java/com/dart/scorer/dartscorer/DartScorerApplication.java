23b24d91e98eecf5a9a6411c2d309afbdc00edd3
chaudharyharsh <chaudharyharsh810@gmail.com>
2024-07-12 12:40:45 +0530

    Initial Commit

---
+++ src/main/java/com/dart/scorer/dartscorer/DartScorerApplication.java
@@ -0,0 +1,13 @@
+package com.dart.scorer.dartscorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DartScorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DartScorerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dart/scorer/dartscorer/DartScorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dart.scorer.dartscorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DartScorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
